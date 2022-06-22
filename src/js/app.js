0ed88808bcb7b2605af7ba3b0aad23754917e67e
Barahten <barakhtenkosm@mchs.ru>
2022-06-23 00:47:00 +0300

    Update app entrypoint

--- src/js/app.js
+++ src/js/app.js
@@ -11,17 +11,45 @@ import Framework7Vue, { registerComponents } from 'framework7-vue/bundle';
 import 'framework7/css/bundle';
 
 // Import Icons and App Custom Styles
-import '../css/icons.css';
 import '../css/app.scss';
 
 // Import App Component
 import App from '../components/app.vue';
+import store from "./store";
 
 // Init Framework7-Vue Plugin
 Framework7.use(Framework7Vue);
 
+// Localization
+import { createI18n } from 'vue-i18n'
+import ru from '../langs/ru.json'
+import en from '../langs/en.json'
+const lang = () => {
+    if(!localStorage.lang) {
+        if(navigator.language === 'ru-RU' || navigator.language === 'ru-ru' || navigator.language === 'ru') {
+            localStorage.lang = 'ru'
+            return 'ru'
+        } else {
+            return 'en'
+        }
+    } else {
+        return localStorage.lang
+    }
+}
+console.log('LANGUAGE', lang())
+const i18n = createI18n({
+    legacy: true,
+    locale: lang(), // set locale
+    fallbackLocale: 'ru', // set fallback locale
+    messages: { ru, en }, // set locale messages
+    // If you need to specify other options, you can set other options
+    // ...
+})
+
 // Init App
 const app = createApp(App);
+app.use(store)
+app.use(i18n)
 
 // Register Framework7 Vue components
 registerComponents(app);
