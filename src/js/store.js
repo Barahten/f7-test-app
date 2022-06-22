c5917cf3a1362f077a9e68be72473d2779d1cf98
Barahten <barakhtenkosm@mchs.ru>
2022-06-23 00:43:26 +0300

    Update store

--- src/js/store.js
+++ src/js/store.js
@@ -1,35 +1,21 @@
 
-import { createStore } from 'framework7/lite';
+import { createStore } from 'vuex'
 
 const store = createStore({
-  state: {
-    products: [
-      {
-        id: '1',
-        title: 'Apple iPhone 8',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
-      },
-      {
-        id: '2',
-        title: 'Apple iPhone 8 Plus',
-        description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
-      },
-      {
-        id: '3',
-        title: 'Apple iPhone X',
-        description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
-      },
-    ]
-  },
-  getters: {
-    products({ state }) {
-      return state.products;
+  state () {
+    return {
+      fontsize: localStorage.fontsize || 14
     }
   },
-  actions: {
-    addProduct({ state }, product) {
-      state.products = [...state.products, product];
-    },
+  getters: {
+    fontsize: (state) => {
+      return state.fontsize
+    }
   },
+  mutations: {
+    fontsize (state, size) {
+      state.fontsize = size
+    }
+  }
 })
 export default store;
